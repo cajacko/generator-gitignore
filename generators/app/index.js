ef023e4a477d838edc8f2b699c31321d7fc9cbd4
Charlie Jackson <charlie@charliejackson.com>
2017-02-23 09:57:10 +0000

    Renamed gitignore template file as was converting to .npmignore automatically

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,7 +3,7 @@ const Generator = require('yeoman-generator');
 module.exports = class extends Generator {
   writing() {
     this.fs.copy(
-      this.templatePath('.gitignore'),
+      this.templatePath('gitignore'),
       this.destinationPath('.gitignore')
     );
   }
